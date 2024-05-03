2c6e48b7a7434feb903fcd290efeb240b0b42ece
jominr <jominr@qq.com>
2024-05-03 15:31:30 +1000

    restful API through dabaseService

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 // import { CreateEmployeeDto } from './dto/create-employee.dto';
 // import { UpdateEmployeeDto } from './dto/update-employee.dto';
@@ -14,8 +14,8 @@ export class EmployeesController {
   }
 
   @Get()
-  findAll() {
-    return this.employeesService.findAll();
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return this.employeesService.findAll(role);
   }
 
   @Get(':id')
