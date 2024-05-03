baf891c47b8ee88ae60058f32190d36ae9bbb3c9
jominr <jominr@qq.com>
2024-05-03 12:05:58 +1000

    users service and update users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,15 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
-
+import { UsersService } from './users.service';
 // controllers are responsible for handing incoming requests
 //  and returning responses to the client. 
 // controllers是用来处理请求的。
 @Controller('users') // /users
 export class UsersController {
-      /* 
+    
+    // 注入我们的UsersService, 
+    constructor(private readonly usersService: UsersService) {}
+      
+    /* 
       GET /users
       GET /users/:id
       POST /users
@@ -14,7 +18,7 @@ export class UsersController {
       */
     @Get() // GET /users or /users?role=value
     findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-      return []
+      return this.usersService.findAll(role);
     }
     // 必须放在/:id之前
     @Get('interns') // GET /users/interns
@@ -25,23 +29,23 @@ export class UsersController {
     @Get(':id') // GET /users/:id
     // param里要传id, id的类型是string
     findOne(@Param('id') id: string) {
-      return { id }
+      return this.usersService.findOne(+id);
     }
 
     @Post() // POST /users
     // body里要传user, user的类型在后面指定
-    create(@Body() user: {}) {
-      return user;
+    create(@Body() user: { name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN'}) {
+      return this.usersService.create(user);
     }
 
     @Patch(':id') // PATCH /users/:id
-    update(@Param('id') id: string, @Body() userUpdate: {}) {
-      return {id, ...userUpdate}
+    update(@Param('id') id: string, @Body() userUpdate: { name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN'}) {
+      return this.usersService.update(+id, userUpdate);
     }
 
     @Delete(':id') // DELETE /users/:id
     delete(@Param('id') id:string) {
-      return { id }
+      return this.usersService.delete(+id);
     }
 
 
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,85 @@
 import { Injectable } from '@nestjs/common';
 
+// providers, 大多数情况是services, 还有可能是
+// repositories, factories, helper, and so on. 
+// the main idea of a provider is that it can be injected as a dependency. 
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      "id": 1,
+      "name": "Leanne Graham",
+      "email": "Sincere@april.biz",
+      "role": "INTERN",
+    },
+    {
+      "id": 2,
+      "name": "Leanne Graham2",
+      "email": "Sincere2@april.biz",
+      "role": "ENGINEER",
+    },
+    {
+      "id": 3,
+      "name": "Leanne Graham3",
+      "email": "Sincere3@april.biz",
+      "role": "INTERN",
+    },
+    {
+      "id": 4,
+      "name": "Leanne Graham4",
+      "email": "Sincere4@april.biz",
+      "role": "ADMIN",
+    },
+    {
+      "id": 5,
+      "name": "Leanne Graham5",
+      "email": "Sincere5@april.biz",
+      "role": "ENGINEER",
+    },
+    {
+      "id": 6,
+      "name": "Leanne Graham6",
+      "email": "Sincere6@april.biz",
+      "role": "ADMIN",
+    },
+  ]
+
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      return this.users.filter(user => user.role === role)
+    }
+    return this.users
+  }
+
+  findOne(id: number) {
+    const user = this.users.find(user => user.id === id)
+    return user;
+  }
+
+  create(user: { name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN'}) {
+    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id) // id从大到小排
+    const newUser = {
+      id: usersByHighestId[0].id + 1,
+      ...user
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(id: number, updatedUser: { name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN'}) {
+    this.users = this.users.map(user => {
+      if (user.id === id) {
+        return {...user, ...updatedUser}
+      }
+      return {...user}
+    })
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOne(id)
+    this.users = this.users.filter(user => user.id !== id);
+    return removedUser;
+  }
+}
